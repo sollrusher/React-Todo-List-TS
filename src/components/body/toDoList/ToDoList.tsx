97c629c0ab0301f689ed8682be54a6d118fc4065
Zandalor-DG <Paliy.zdg@gmail.com>
2020-11-21 18:57:35 +0300

    draft

---
+++ src/components/body/toDoList/ToDoList.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+const ToDoList = (): JSX.Element => {
+    return (
+        <section className="toDoList">
+            <input type="checkbox" className="toDoList__toggle-all" id="toggle-all" />
+            <label htmlFor="toggle-all">Mark all as complete</label>
+            <ul className="toDoList__list">
+                <li className="toDoList__item">
+                    <div className="toDoList__view">
+                        <input type="checkbox" className="toDoList__toggle" />
+                        <label className="toDoList__text"></label>
+                        <button className="toDoList__destroy"></button>
+                    </div>
+                </li>
+            </ul>
+        </section>
+    );
+};
+export default ToDoList;
---
+++ src/components/header/HeaderContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from './Header';
+
+const HeaderContainer = (): JSX.Element => {
+    return <Header />;
+};
+
+export default HeaderContainer;
