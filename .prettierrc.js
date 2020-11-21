5d571f673bde47e694894f8d853bd11b5ed95cad
Zandalor-DG <Paliy.zdg@gmail.com>
2020-11-21 16:30:53 +0300

    added ESlint amd Prettier to project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    endOfLine: 'auto',
+    'prettier/prettier': ['error', { singleQuote: true, parser: 'flow' }],
+};
